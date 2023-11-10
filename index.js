12c2d170ef46391b94f829d4c443cc266f9bdb78
ViniHackerman <viniciusgouveamachado@gmail.com>
2023-11-09 22:26:37 -0300

    Criação - primeira view do projeto

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
 app.get('/', (requisicao, resposta) =>{
-    resposta.send("Ola mundo")
+    resposta.render('home')
 })
 
 app.listen(3000, () => {
